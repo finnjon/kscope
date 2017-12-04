0af89dac3d2e3ac0ae17d93a7cbe94f897435f5e
Finnjon <jonathondsmartin@gmail.com>
2017-12-04 18:40:39 +0200

    fix url for sending info and revert profile html to old way

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -33,7 +33,7 @@ export class ProfileComponent implements OnInit {
     submission.profile = this.profileService.profile;
     submission.feedback = this.feedback;
     console.log(submission);
-    this.http.post('https://jons-email-server.appspot.com/sendProfile', submission)
+    this.http.post('http://kaleidoscope.helsinki.fi/sendProfile', submission)
       .subscribe(
         (response) => console.log("message sent"),
         (error) => console.log("message failed"),
